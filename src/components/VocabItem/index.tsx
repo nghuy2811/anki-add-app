40a7f6a24716be7f1c149ceb728b7600b17755f1
Huy Nguyen <nghuy2811@gmail.com>
2023-06-06 11:24:26 +0700

    Get front and back card data

--- src/components/VocabItem/index.tsx
+++ src/components/VocabItem/index.tsx
@@ -1,25 +1,54 @@
 'use client';
 
-import React, { useEffect } from 'react';
+import React, { useCallback, useMemo } from 'react';
 
 import { IVocab } from '@/dictionary/merriam.dictionary';
+import Button from '../Button';
 
 type VocabItemPropTypes = {
   data: IVocab;
 };
 
 const VocabItem = ({ data }: VocabItemPropTypes) => {
-  useEffect(() => {
-    console.log(data);
-  }, [data]);
+  const mainWord = useMemo(
+    () => data.hwi.hw.replace(/[*]/g, ''),
+    [data.hwi.hw]
+  );
+  const handleAddToAnki = useCallback(() => {
+    const front = `<div class='front'><h1 class='word'>${mainWord}</h1><span class='type'>(${data.fl})</span></div>`;
+    let definitions = '';
+
+    for (let index = 0; index < data.shortdef.length; index++) {
+      const def = data.shortdef[index];
+      const defElement = `<li>${def}</li>`;
+      definitions = definitions.concat(defElement);
+    }
+
+    const back = `<div class='back'><ul class='definitions'>${definitions}</ul></div>`;
+
+    console.log({
+      front: front.replace(/\n/g, ''),
+      back,
+    });
+  }, [data, mainWord]);
+
   return (
-    <div className='m-[5px] border'>
-      <h2>{data.hwi.hw.replace(/[*]/g, '')}</h2>
-      <h4>{data.fl}</h4>
-      <div>
-        {data.shortdef.map((def, index) => (
-          <li key={index}>{def}</li>
-        ))}
+    <div className='my-[10px] rounded-[10px] border bg-white px-[10px] py-[20px]'>
+      <h2 className='text-[24px] font-[700] leading-[40px]'>{mainWord}</h2>
+      <h4 className='text-[16px] font-[500] leading-[20px] text-blue'>
+        {data.fl}
+      </h4>
+      {data.shortdef.length > 0 && (
+        <ul className='ml-[16px] pt-[16px]'>
+          {data.shortdef.map((def, index) => (
+            <li key={index} className='list-decimal text-[18px] leading-[26px]'>
+              {def}
+            </li>
+          ))}
+        </ul>
+      )}
+      <div className='mt-[20px] flex justify-center'>
+        <Button label='Add to Anki' action={handleAddToAnki} />
       </div>
     </div>
   );
